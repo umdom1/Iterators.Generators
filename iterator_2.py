c5657a2f3cb87573055562bc759f0114370e0632
Denis Denisov <umdom1@gmail.com>
2023-09-17 19:53:02 +0300

    First commit

---
+++ iterator_2.py
@@ -0,0 +1,37 @@
+import types
+
+
+def flat_generator(list_of_lists):
+    for item in list_of_lists:
+        for el in item:
+            yield el
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+    print(list(flat_generator(list_of_lists_1)))
+
+
+
+if __name__ == '__main__':
+    test_2()
+
+
+
+
